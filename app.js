fb82f36d5f8befd8d0575c2263aa580f651a52d7
serg1557733 <serg1557733@ukr.net>
2021-04-23 16:16:24 +0300

    second commit

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 
-let button = document.querySelector('button');
-let input = document.querySelector('input').value;
-console.log(input)
-button.onclick = () =>console.log(input);
+let input = document.getElementsByClassName('#main_input').input;
+console.log(input);
+let btn = document.getElementsByTagName('button')
+btn.onclick = () => console.log('work');
+
+
 
 let a = '123321'
 let arr = a.split('').reverse().join('');
-
-console.log(a)
-console.log(arr)
-
+console.log('intresult a'+' '+ a)
+console.log('result arr'+' '+arr)
 let str1 = 'aNna'
 function polindrom (str){
     str = str.toLowerCase()
  if (str===str.split('').reverse().join('')){return true}
 }
-
-console.log(polindrom(str1))
+console.log('it`s polindrome'+' '+polindrom(str1))
