23783da2abd455d8e92c75bd3086136809344710
serg1557733 <serg1557733@ukr.net>
2021-04-24 11:50:54 +0300

    geiby id

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 
-let input = document.getElementsByClassName('main_input').input;
-console.log(input);
-let btn = document.getElementsByTagName('button')
-btn.onclick = () => console.log('work');
-
-
+let input = document.getElementById('main_input');
+console.log(input.value);
+document.getElementById('btn').onclick = () => console.log('work');;
+ 
 
 let a = '123321'
 let arr = a.split('').reverse().join('');
 console.log('intresult a'+' '+ a)
 console.log('result arr'+' '+arr)
+
+
 let str1 = 'aNna'
 function polindrom (str){
     str = str.toLowerCase()
