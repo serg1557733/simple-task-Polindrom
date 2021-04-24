cf5806e1568e4a4d2e14785691d10ce731cb9afc
serg1557733 <serg1557733@ukr.net>
2021-04-24 12:13:34 +0300

    change 2

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-
-<<<<<<< HEAD
 let input = document.getElementById('main_input');
-document.getElementById('btn').onclick = () => console.log(input.value);;
+document.getElementById('btn').onclick = () => {
+    console.log(input.value)
+    input.value = ''};
+
  
-=======
-let input = document.getElementsByClassName('main_input').input;
-console.log(input);
-let btn = document.getElementsByTagName('button')
-btn.onclick = () => console.log('work');
->>>>>>> c75764413a8166bdd90d392e45f81ee72080c6eb
 
 let a = '123321';
 let arr = a.split('').reverse().join('');
@@ -16,9 +11,10 @@ console.log('intresult a'+' '+ a)
 console.log('result arr'+' '+arr)
 
 
+
 let str1 = 'aNna'
 function polindrom (str){
     str = str.toLowerCase()
- if (str===str.split('').reverse().join('')){return true}
-}
+    if (str===str.split('').reverse().join('')){return true}
+    }
 console.log('it`s polindrome'+' '+polindrom(str1))
