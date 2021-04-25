6802b45dd0b363ce60e92be4ae08844f3f9d170c
serg1557733 <serg1557733@ukr.net>
2021-04-25 17:12:44 +0300

    four

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.getElementById('btn').onclick = () => {
     input.value = ''};
 
  
-
+//test function
 let a = '123321';
 let arr = a.split('').reverse().join('');
 console.log('intresult a'+' '+ a)
@@ -12,9 +12,14 @@ console.log('result arr'+' '+arr)
 
 
 
-let str1 = 'aNna'
+let str1 = 'aNna1'
+document.getElementById('btn1').onclick = polindrom (str1);
+
 function polindrom (str){
     str = str.toLowerCase()
-    if (str===str.split('').reverse().join('')){return true}
-    }
-console.log('it`s polindrome'+' '+polindrom(str1))
+    if (str===str.split('').reverse().join(''))
+    {return console.log('it`s polindrome')}
+    else {console.log('it`s not polindrome')}
+    };
+    
+    console.log('work end');
